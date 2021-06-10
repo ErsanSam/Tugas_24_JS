9d5dbaaca4b33475ca454d74e512ef68f07b2656
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-10 14:18:55 +0700

    Tugas 24 JS
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,9 @@
+function panggilSort(){
+  var number =[32,33,16,40,9,7,11,2,44,1,66];
+  console.log(number);
+  number = number.sort();
+  console.log(number)
+  return number;
+}
+console.log(panggilSort().reverse());
+// panggilSort().reverse()
